1f1df20c8f3cf58bdd13663c78eb770e3cab1e68
Kaique Nocetti <kaique.nocetti@medportal.com>
2025-05-27 01:16:27 -0300

    fix: ensure MYSQLPORT is correctly parsed as a number for database connection

--- models/db.js
+++ models/db.js
@@ -4,7 +4,7 @@ dotenv.config({ path: 'api/.env' });
 
 export const db = await mysql.createPool({
   host: process.env.MYSQLHOST,
-  port: process.env.MYSQLPORT,
+  port: Number(process.env.MYSQLPORT),
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
